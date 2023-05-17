4eb8fd6b529f0d7bc6eb2898bac86158381903ee
lghanotakis <lea.ghanotakis@epfedu.fr>
2023-05-17 16:10:55 +0200

    Commit 2 - Affichage des affiches de popular movies

--- app/src/main/java/fr/epf/mm/cinemathome/MainActivity.kt
+++ app/src/main/java/fr/epf/mm/cinemathome/MainActivity.kt
@@ -1,33 +1,53 @@
 package fr.epf.mm.cinemathome
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import fr.epf.mm.cinemathome.APITMDB.Api
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    val page = 1
     val apiKey = "5b6adc52282ae68909484fb3fcd7cd07"
     val language = "en-US"
-    val category = "popular"
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MovieAdapter
+
+    lateinit var test : RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MovieRepository.getPopularMovies()
+        test = findViewById(R.id.test_recyclerview)
+        test.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-    /*    val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val service = retrofit.create(Api::class.java)
-        Call<MovieResult> Call = service.listOfMovies(category, apiKey, language, page)*/
+
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMoviesAdapter = MovieAdapter(listOf())
+        popularMovies.adapter = popularMoviesAdapter
+
+        MovieRepository.getPopularMovies(
+            onSuccess = ::onPopularMovieFetched,
+            onError = ::onError
+        )
+
     }
 
+    private fun onPopularMovieFetched(movies: List<Movie>) {
+        popularMoviesAdapter.updateMovies(movies)    }
 
+    private fun onError() {
+        Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+    }
 
 }
